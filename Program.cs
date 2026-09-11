c8e2c07b1eb96ef9a4b5ba8eee70408609c3e3c0
SkriptIsHere <fab123cast@gmail.com>
2026-09-11 16:07:17 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,73 @@
-﻿
-int Score = 0;
-int EndScore = 0;
-bool Weiter = true;
+﻿using System.Threading;
 
-do
+class QuizMaster
 {
-    //Titel
-    Console.SetCursorPosition(71, 4);
-    Console.WriteLine("================");
-    Console.SetCursorPosition(71, 5);
-    Console.WriteLine("  QUIZ MASTERS");
-    Console.SetCursorPosition(71, 6);
-    Console.WriteLine("================");
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.SetCursorPosition(72, 13);
+        Console.Write("QUIZ MASTERS");
 
+        Console.SetCursorPosition(72, 16);
+        Console.Write("Start Game[1]");
 
-    //Alle Fragen and Antworten werden in ein Array gespeichert
-    Console.SetCursorPosition(55, 8);
+        Console.SetCursorPosition(72, 17);
+        Console.Write("Exit[2]");
+
+        Console.SetCursorPosition(72, 20);
+        Console.Write("> ");
+
+        ConsoleKeyInfo input = Console.ReadKey();
+
+        if (input.Key == ConsoleKey.D1)
+        {
+            Console.Clear();
+            Console.ResetColor();
+            Loading();
+            Thread.Sleep(1000);
+            Console.Clear();
+            Quiz();
+                
+        }
+        else if (input.Key == ConsoleKey.D2)
+            {
+
+            Environment.Exit(0);
+        }
+
+        
+
+    }
+
+    static void Loading()
+    {
+        string text = "Loading...";
+
+        Console.SetCursorPosition(73, 15);
+        foreach (char Buchstabe in text)
+        {
+            Console.Write(Buchstabe);
+            Thread.Sleep(100);
+
+        }
+
+    }
+
+    static void Quiz()
+    {
+
+    int Score = 0;
+    int EndScore = 0;
+    bool Weiter = true;
+    int letzteFrage = -1;
+
+    Random random = new Random();
+
     string[] questions =
     {
-    "     Wie heißt die Hauptstadt von Deutschland? ", "       Wie heißt der höchste Berg der Welt? ",
-    "   Welcher Planet ist der Sonne am nächsten? ", "Welches Tier wird oft als „König der Tiere“ bezeichnet?  ", "     Wie viele Planeten hat unser Sonnensystem",
-    "   Welches Land hat die meisten Einwohner der Welt?", "Welcher Komponist schuf das berühmte Werke wie die 9. Sinfonie?", "      Welches Land hat die meisten Inseln?", "    Welches Land hat die Form eines Stiefels?"
+    "          Wie heißt die Hauptstadt von Deutschland? ", "            Wie heißt der höchste Berg der Welt? ",
+    "         Welcher Planet ist der Sonne am nächsten? ", "  Welches Tier wird oft als „König der Tiere“ bezeichnet?  ", "          Wie viele Planeten hat unser Sonnensystem",
+    "        Welches Land hat die meisten Einwohner der Welt?", "Welcher Komponist schuf das berühmte Werke wie die 9. Sinfonie?", "           Welches Land hat die meisten Inseln?", "         Welches Land hat die Form eines Stiefels?"
 
 };
 
@@ -29,38 +76,84 @@ do
 
 };
 
+    do
+{
+    //Titel
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.SetCursorPosition(70, 4);
+    Console.WriteLine("================");
+    Console.SetCursorPosition(70, 5);
+    Console.WriteLine("  QUIZ MASTERS");
+    Console.SetCursorPosition(70, 6);
+    Console.WriteLine("================");
+    Console.ResetColor();
+
+
+    //Alle Fragen and Antworten werden in ein Array gespeichert
+    Console.ForegroundColor= ConsoleColor.Yellow;
+    Console.SetCursorPosition(50, 8);
+    
+
     //Die Zufallsfunktion
-    Random random = new Random();
     int QuestionRandomizer = random.Next(questions.Length);
 
 
+ // Verhindert doppel-Fragen
+    while (letzteFrage == QuestionRandomizer)
+    {
+          QuestionRandomizer = random.Next(questions.Length);
+    }
+
+          letzteFrage = QuestionRandomizer;
+
+
+    // Schreibt die zufallige Frage
     Console.WriteLine(questions[QuestionRandomizer]);
 
+
     //antworten
-    Console.SetCursorPosition(74, 12);
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.SetCursorPosition(72, 12);
+    Console.Write("> ");
     string answer = Console.ReadLine();
 
-    //Prüfe mit der passenden Antwort + Score
-    if (answer == answers[QuestionRandomizer])
-    {
+    //Prüfe mit der passenden Antwort + Score ( || = oder, && = und)
+    if (answer.Equals(answers[QuestionRandomizer], StringComparison.OrdinalIgnoreCase)
+    || (QuestionRandomizer == 4 && answer == "8"))
+            {
         Console.WriteLine("Richtig!");
         Score = Score + 1;
         Console.Clear();
-      
+
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Dein Score ist " + Score);
+        Console.ResetColor();
              
 
             }
     else
     {
+        Console.ForegroundColor= ConsoleColor.Red;
         Console.WriteLine("Falsch! Richtige Antwort: " + answers[QuestionRandomizer]);
         EndScore = Score;
     Weiter = false;
 
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Dein Endscore ist " + EndScore);
+        Console.ResetColor();
     }
 }
 while (Weiter == true);
+
+    }
+
+   
+}
+
+
+
+
+
 
 
 
